8f5ecdfd4c2d526a71129adb078745e1fbd97eb7
Agatha147 <agatha.nonato.almeida@escola.pr.gov.br>
2025-08-19 12:13:46 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,73 +1,99 @@
 criaCartao(
     'Disney',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Onde surgiu o conto "A Bela e a Fera"?',
+    'O conto surgiu na França em 1740, escrito por 
+    'Gabrielle-Suzanne Barbot de Villeneuve, 
+     'sendo depois adaptado por Jeanne-Marie Leprince de Beaumont.'
 )
 
 criaCartao(
     'Disney',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual o filme em que a protagonista perde o sapato?',
+    'Cinderella perdeu o sapato de cristal no baile ao sair apressada, 
+    'e foi assim que o príncipe conseguiu encontrá-la.'
 )
 
 criaCartao(
     'Disney',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Qual o nome da fada malvada de A Bela Adormecida?',
+    'Malévola, a fada malvada, era uma antiga amiga de Fauna,
+    'Flora e Primavera, mas se voltou contra o reino e lançou
+    'a maldição na princesa Aurora.'
 )
 
 criaCartao(
     'Disney',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Quem morre em O Rei Leão?',
+    'Mufasa foi morto pelo irmão Scar,
+    'em uma emboscada nas Gargantas, 
+    'marcando um dos momentos mais tristes
+    'da animação.'
 )
 criaCartao(
     'Disney',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
-criaCartao(
-    'Disney',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
-)
-
-criaCartao(
-    'Disney',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Qual o país que Wallace Coração Bravo nasceu?',
+    'William Wallace, conhecido como "Coração Valente", 
+    'nasceu na Escócia e se tornou um herói nacional na
+    'luta pela liberdade do povo escocês.'
 )
 
 criaCartao(
     'Disney',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-
-criaCartao(
-    'Disney',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
-criaCartao(
-    'Disney',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Quem é o vilão de O Corcunda de Notre Dame?,
+    'Claude Frollo é o vilão de "O Corcunda de Notre Dame",
+    'juiz implacável que governa Paris com tirania e preconceito'
 )
 
 criaCartao(
     'Disney',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Quem é a órfã em Anastácia?',
+    'Anya, uma jovem órfã russa, é a verdadeira princesa Anastácia 
+    'que perdeu sua família durante a revolução e busca reencontrar 
+    'suas origens.
+)
+
+criaCartao(
+    'Disney',
+    'Qual o nome da protagonista de Valente?',
+    'Merida é a princesa escocesa de "Valente",
+    'conhecida por sua coragem, espírito livre e 
+    'habilidade com o arco e flecha.'
+
+criaCartao(
+    'Disney',
+    'Qual o nome do pires em A Bela e a Fera?',
+    ' Zip é o pequeno pires encantado, filho da Madame Samovar 
+    '(a chaleira), que também foi transformado por causa da
+    'maldição do castelo.
+)
+criaCarta(
+    'Disney',
+    'Qual o nome do sapo em "A Princesa e o Sapo"?',
+    'Naveen é o príncipe transformado em sapo no filme
+    '"A Princesa e o Sapo", e junto com Tiana, 
+    'vive a aventura para quebrar o feitiço.'
+)
+
+criaCartao(
+    'Disney',
+    'O que significa "OHANA"?',
+    '"Ohana" significa família em havaiano,
+    'e como diz a Lilo em "Lilo & Stitch": 
+    'Ohana quer dizer família. Família quer 
+    'dizer nunca abandonar ou esquecer.'
+    
 )
 
 criaCartao(
     'Disney,
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Qual o nome do peixe amigo da Ariel em "A Pequena Sereia"?',
+    'Linguado é o peixe amigo fiel da Ariel em "A Pequena Sereia", 
+    'sempre a acompanhando em suas aventuras no fundo do mar.'
 )
 
 criaCartao(
     'Disney,
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Quem dá vida aos brinquedos em "Toy Story"?',
+    'Em "Toy Story", os brinquedos ganham vida sempre que os humanos
+    'não estão olhando, revelando um mundo secreto cheio de amizade
+    'e descobertas.
